099c735c176adb01ab6cd8e8ac1e7e5fa5a2b55e
Dominik Mlynka <dominik.mlynka@gmail.com>
2021-05-03 16:22:08 +0200

    first successful build and try of basic input, output operations

---
+++ components/iocontrollers/iocontrollers.c
@@ -0,0 +1,78 @@
+#include "iocontrollers.h"
+
+const Output RELAY1 = {
+    .pinNumber = RELAY_1_PIN,
+    .switchMode = SWITCH_MODE_SIMPLE,
+    .delayMicros = 0
+};
+
+const Output RELAY2 = {
+    .pinNumber = RELAY_2_PIN,
+    .switchMode = SWITCH_MODE_SIMPLE,
+    .delayMicros = 0
+};
+
+const Output OPTO1 = {
+    .pinNumber = OPTO_1_PIN,
+    .switchMode = SWITCH_MODE_SIMPLE,
+    .delayMicros = 0
+};
+
+const Output OPTO2 = {
+    .pinNumber = OPTO_2_PIN,
+    .switchMode = SWITCH_MODE_SIMPLE,
+    .delayMicros = 0
+};
+
+const Input SW1 = {
+    .pinNumber = SW_1_PIN,
+    .pullUp = true
+};
+
+const Input SW2 = {
+    .pinNumber = SW_2_PIN,
+    .pullUp = true
+};
+
+const Input INPUT1 = {
+    .pinNumber = INPUT_1_PIN,
+    .pullUp = true
+};
+
+const Input INPUT2 = {
+    .pinNumber = INPUT_2_PIN,
+    .pullUp = true
+};
+
+esp_err_t output_activate(const Output *output, const uint8_t value) {
+    return gpio_set_level(output->pinNumber, value);
+}
+
+int input_read(const Input *input) {
+    return gpio_get_level(input->pinNumber);
+}
+
+void set_output_switch_mode(Output *output, const SwitchMode mode) {
+    output->switchMode = mode;
+}
+
+void init_io_controller() {
+    //set up outputs
+    gpio_config_t config;
+    config.intr_type = GPIO_INTR_DISABLE;
+    config.pin_bit_mask = (1U << RELAY1.pinNumber) | (1U << RELAY2.pinNumber) | (1U << OPTO1.pinNumber) | (1U << OPTO2.pinNumber);
+    config.mode = GPIO_MODE_OUTPUT;
+    config.pull_up_en = 0;
+    config.pull_down_en = 0;
+
+    gpio_config(&config);
+
+    //set up inputs
+    config.intr_type = GPIO_INTR_DISABLE;
+    config.pin_bit_mask = (1ULL << INPUT1.pinNumber) | (1ULL << INPUT2.pinNumber) | (1ULL << SW1.pinNumber) | (1ULL << SW2.pinNumber);
+    config.mode = GPIO_MODE_INPUT;
+    config.pull_up_en = 0;
+    config.pull_down_en = 0;
+
+    gpio_config(&config);
+}
--- main/garage_controller.c
+++ main/garage_controller.c
@@ -1,6 +1,30 @@
 #include <stdio.h>
-#include <gpio.h>
+#include "iocontrollers.h"
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
 
 void app_main(void)
 {
+    init_io_controller();
+    output_activate(&RELAY1, 1);
+    output_activate(&RELAY2, 1);
+    output_activate(&OPTO1, 1);
+
+    while(1) {
+        printf("pinumber[%d] = %d\n", INPUT1.pinNumber, input_read(&INPUT1));
+        printf("pinumber[%d] = %d\n", INPUT2.pinNumber, input_read(&INPUT2));
+        printf("pinumber[%d] = %d\n", SW1.pinNumber, input_read(&SW1));
+        printf("pinumber[%d] = %d\n\n", SW2.pinNumber, input_read(&SW2));
+        if (input_read(&SW1) == 0) {
+            output_activate(&RELAY1, 1);
+        } else {
+            output_activate(&RELAY1, 0);
+        }
+        if (input_read(&SW2) == 0) {
+            output_activate(&RELAY2, 1);
+        } else {
+            output_activate(&RELAY2, 0);
+        }
+        vTaskDelay(1000 / portTICK_RATE_MS);
+    }
 }
