f20b535d967dc25b70d5a9b914f9ea16e9dcedb8
Dominik Mlynka <dominik.mlynka@gmail.com>
2021-05-17 19:10:00 +0200

    mqqt handler moved to dedicated file

--- main/garage_controller.c
+++ main/garage_controller.c
@@ -1,4 +1,3 @@
-#include <stdio.h>
 #include "esp_eth.h"
 #include "esp_event.h"
 #include "esp_log.h"
@@ -8,95 +7,15 @@
 #include "freertos/task.h"
 #include "iocontrollers.h"
 #include "manual_input_event_handler.h"
-#include "mqtt_client.h"
 #include "mqtt_handler.h"
 #include "nvs_flash.h"
 #include "temperature.h"
 #include "driver/gpio.h"
 #include "sdkconfig.h"
+#include <stdio.h>
 #include "wifi_handler.h"
 
 static const char* TAG = "garage_controller";
-
-static void log_error_if_nonzero(const char *message, int error_code)
-{
-    if (error_code != 0) {
-        ESP_LOGE(TAG, "Last error %s: 0x%x", message, error_code);
-    }
-}
-
-static void mqtt_event_handler(void *handler_args, esp_event_base_t base, int32_t event_id, void *event_data)
-{
-    ESP_LOGD(TAG, "Event dispatched from event loop base=%s, event_id=%d", base, event_id);
-    esp_mqtt_event_handle_t event = event_data;
-    esp_mqtt_client_handle_t client = event->client;
-    int msg_id;
-    switch ((esp_mqtt_event_id_t)event_id) {
-    case MQTT_EVENT_CONNECTED:
-        ESP_LOGI(TAG, "MQTT_EVENT_CONNECTED");
-        msg_id = esp_mqtt_client_publish(client, "6048e887d1c2d77d1bd58f16/report/online", "true", 0, 1, 1);
-        ESP_LOGI(TAG, "sent publish successful, msg_id=%d", msg_id);
-
-        msg_id = esp_mqtt_client_subscribe(client, "6048e887d1c2d77d1bd58f16/directive/powerState", 1);
-        ESP_LOGI(TAG, "sent subscribe successful, msg_id=%d", msg_id);
-        break;
-    case MQTT_EVENT_DISCONNECTED:
-        ESP_LOGI(TAG, "MQTT_EVENT_DISCONNECTED");
-        break;
-    case MQTT_EVENT_SUBSCRIBED:
-        ESP_LOGI(TAG, "MQTT_EVENT_SUBSCRIBED, msg_id=%d", event->msg_id);
-        break;
-    case MQTT_EVENT_UNSUBSCRIBED:
-        ESP_LOGI(TAG, "MQTT_EVENT_UNSUBSCRIBED, msg_id=%d", event->msg_id);
-        break;
-    case MQTT_EVENT_PUBLISHED:
-        ESP_LOGI(TAG, "MQTT_EVENT_PUBLISHED, msg_id=%d", event->msg_id);
-        break;
-    case MQTT_EVENT_DATA:
-        ESP_LOGI(TAG, "MQTT_EVENT_DATA");
-        printf("TOPIC=%.*s\r\n", event->topic_len, event->topic);
-        printf("DATA=%.*s\r\n", event->data_len, event->data);
-        break;
-    case MQTT_EVENT_ERROR:
-        ESP_LOGI(TAG, "MQTT_EVENT_ERROR");
-        if (event->error_handle->error_type == MQTT_ERROR_TYPE_TCP_TRANSPORT) {
-            log_error_if_nonzero("reported from esp-tls", event->error_handle->esp_tls_last_esp_err);
-            log_error_if_nonzero("reported from tls stack", event->error_handle->esp_tls_stack_err);
-            log_error_if_nonzero("captured as transport's socket errno",  event->error_handle->esp_transport_sock_errno);
-            ESP_LOGI(TAG, "Last errno string (%s)", strerror(event->error_handle->esp_transport_sock_errno));
-
-        }
-        break;
-    default:
-        ESP_LOGI(TAG, "Other event id:%d", event->event_id);
-        break;
-    }
-}
-
-
-static void mqtt_app_start(void)
-{
-    esp_mqtt_client_config_t mqtt_cfg = {
-        .uri = "mqtt://192.168.1.22:1883",
-        .client_id = "6048e887d1c2d77d1bd58f16",
-        .lwt_msg = "false",
-        .lwt_msg_len = 0,
-        .lwt_qos = 2,
-        .lwt_retain = 1,
-        .lwt_topic = "6048e887d1c2d77d1bd58f16/report/online",
-        .keepalive = 10
-    };
-    esp_mqtt_client_handle_t client = esp_mqtt_client_init(&mqtt_cfg);
-    esp_mqtt_client_register_event(client, ESP_EVENT_ANY_ID, mqtt_event_handler, NULL);
-    esp_mqtt_client_start(client);
-}
-
-static void mqtt_got_ip_handler(void *arg, esp_event_base_t event_base,
-                                 int32_t event_id, void *event_data)
-                                 {
-                                     ESP_LOGI(TAG, "trying to start mqtt broker");
-                                     mqtt_app_start();
-                                 }
 
 void app_main(void)
 {
@@ -107,7 +26,8 @@ void app_main(void)
     ESP_ERROR_CHECK(esp_event_loop_create_default());
 
     ethernet_init();
-    ESP_ERROR_CHECK(esp_event_handler_instance_register(IP_EVENT, IP_EVENT_ETH_GOT_IP, &mqtt_got_ip_handler, NULL, NULL));
+    mqtt_app_start();
+    //ESP_ERROR_CHECK(esp_event_handler_instance_register(IP_EVENT, IP_EVENT_ETH_GOT_IP, &mqtt_got_ip_handler, NULL, NULL));
     //wifi_init_sta();
     //io_controllers_init();
     //manual_input_event_handler_init();
