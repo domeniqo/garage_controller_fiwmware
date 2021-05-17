30ed8c4a330a68ed3d8559370ce5a9f06358fc3a
Dominik Mlynka <dominik.mlynka@gmail.com>
2021-05-17 19:10:00 +0200

    mqqt handler moved to dedicated file

--- components/iot/mqtt_handler.c
+++ components/iot/mqtt_handler.c
@@ -1,1 +1,97 @@
+#include "esp_eth.h"
+#include "esp_event.h"
+#include "esp_log.h"
+#include "esp_netif.h"
+#include "mqtt_client.h"
 #include "mqtt_handler.h"
+#include <stdio.h>
+
+static const char* temp = "6050bdd81d065424b548c632";
+static const char* relay1 = "6048e887d1c2d77d1bd58f16";
+static const char* door_magnet = "604907d8d1c2d77d1bd58f1f";
+
+static const char* TAG = "mqtt_handler";
+
+void log_error_if_nonzero(const char *message, int error_code)
+{
+    if (error_code != 0) {
+        ESP_LOGE(TAG, "Last error %s: 0x%x", message, error_code);
+    }
+}
+
+void mqtt_event_handler(void *handler_args, esp_event_base_t base, int32_t event_id, void *event_data)
+{
+    ESP_LOGD(TAG, "Event dispatched from event loop base=%s, event_id=%d", base, event_id);
+    esp_mqtt_event_handle_t event = event_data;
+    esp_mqtt_client_handle_t client = event->client;
+    int msg_id;
+    switch ((esp_mqtt_event_id_t)event_id) {
+    case MQTT_EVENT_CONNECTED:
+        ESP_LOGI(TAG, "MQTT_EVENT_CONNECTED");
+        char topic[128];
+        strcpy(topic, relay1);
+        msg_id = esp_mqtt_client_publish(client, strcat(topic, "/report/online"), "true", 0, 1, 1);
+        ESP_LOGI(TAG, "sent publish successful, msg_id=%d", msg_id);
+        strcpy(topic, temp);
+        msg_id = esp_mqtt_client_publish(client, strcat(topic, "/report/online"), "true", 0, 1, 1);
+        ESP_LOGI(TAG, "sent publish successful, msg_id=%d", msg_id);
+        strcpy(topic, door_magnet);
+        msg_id = esp_mqtt_client_publish(client, strcat(topic, "/report/online"), "true", 0, 1, 1);
+        ESP_LOGI(TAG, "sent publish successful, msg_id=%d", msg_id);
+
+        strcpy(topic, relay1);
+        msg_id = esp_mqtt_client_subscribe(client, strcat(topic, "/directive/powerState"), 1);
+        ESP_LOGI(TAG, "sent subscribe successful, msg_id=%d", msg_id);
+        break;
+    case MQTT_EVENT_DISCONNECTED:
+        ESP_LOGI(TAG, "MQTT_EVENT_DISCONNECTED");
+        break;
+    case MQTT_EVENT_SUBSCRIBED:
+        ESP_LOGI(TAG, "MQTT_EVENT_SUBSCRIBED, msg_id=%d", event->msg_id);
+        break;
+    case MQTT_EVENT_UNSUBSCRIBED:
+        ESP_LOGI(TAG, "MQTT_EVENT_UNSUBSCRIBED, msg_id=%d", event->msg_id);
+        break;
+    case MQTT_EVENT_PUBLISHED:
+        ESP_LOGI(TAG, "MQTT_EVENT_PUBLISHED, msg_id=%d", event->msg_id);
+        break;
+    case MQTT_EVENT_DATA:
+        ESP_LOGI(TAG, "MQTT_EVENT_DATA");
+        printf("TOPIC=%.*s\r\n", event->topic_len, event->topic);
+        printf("DATA=%.*s\r\n", event->data_len, event->data);
+        break;
+    case MQTT_EVENT_ERROR:
+        ESP_LOGI(TAG, "MQTT_EVENT_ERROR");
+        if (event->error_handle->error_type == MQTT_ERROR_TYPE_TCP_TRANSPORT) {
+            log_error_if_nonzero("reported from esp-tls", event->error_handle->esp_tls_last_esp_err);
+            log_error_if_nonzero("reported from tls stack", event->error_handle->esp_tls_stack_err);
+            log_error_if_nonzero("captured as transport's socket errno",  event->error_handle->esp_transport_sock_errno);
+            ESP_LOGI(TAG, "Last errno string (%s)", strerror(event->error_handle->esp_transport_sock_errno));
+        }
+        break;
+    default:
+        ESP_LOGI(TAG, "Other event id:%d", event->event_id);
+        break;
+    }
+}
+
+void mqtt_app_start(void)
+{
+    char topic[128];
+    strcpy(topic, relay1);
+    esp_mqtt_client_config_t mqtt_cfg = {
+        .uri = "mqtt://3.122.209.170:1883",
+        .username = "domeniqo",
+        .password = "Sasenka1",
+        .client_id = relay1,
+        .lwt_msg = "false",
+        .lwt_msg_len = 0,
+        .lwt_qos = 2,
+        .lwt_retain = 1,
+        .lwt_topic = strcat(topic, "/report/online"),
+        .keepalive = 10
+    };
+    esp_mqtt_client_handle_t client = esp_mqtt_client_init(&mqtt_cfg);
+    esp_mqtt_client_register_event(client, ESP_EVENT_ANY_ID, mqtt_event_handler, NULL);
+    esp_mqtt_client_start(client);
+}
