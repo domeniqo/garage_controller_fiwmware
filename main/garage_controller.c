a732363c6d92e0b0f82fa349a335a18bdb23bd2f
Dominik Mlynka <dominik.mlynka@gmail.com>
2021-05-17 19:15:00 +0200

    renamed mqqt_init function

--- main/garage_controller.c
+++ main/garage_controller.c
@@ -25,14 +25,14 @@ void app_main(void)
     ESP_ERROR_CHECK(esp_netif_init());
     ESP_ERROR_CHECK(esp_event_loop_create_default());
 
-    ethernet_init();
-    mqtt_app_start();
+    //ethernet_init();
+    wifi_init_sta();
+    mqtt_init();
     //ESP_ERROR_CHECK(esp_event_handler_instance_register(IP_EVENT, IP_EVENT_ETH_GOT_IP, &mqtt_got_ip_handler, NULL, NULL));
-    //wifi_init_sta();
     //io_controllers_init();
     //manual_input_event_handler_init();
 
-    //mqtt_app_start();
+    //mqtt_init();
     while(1) {
         ESP_LOGI(TAG, "main checking");
         vTaskDelay(500);
